56ddf48aa2c5714d47c7d84e15d0c44c731c54fd
James Pedersen <james00090@gmail.com>
2020-07-24 02:16:23 +0100

    got dropdown colorwheel showing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import Palette from "./Palette";
 import Settings from "./Navbar";
+import $ from "jquery";
 
 function App() {
   const [hex, changeHex] = useState("#5f42ad");
   const [variation, changeVariation] = useState("analogous");
   const [setting, changeSetting] = useState();
   const [settingValue, changeSettingValue] = useState(20);
+  const [colorWheelEnabled, setColorWheelEnabled] = useState(false);
 
   return (
     <div className="App">
@@ -16,12 +18,15 @@ function App() {
         changeVariation={changeVariation}
         changeHex={changeHex}
         changeSettingValue={changeSettingValue}
+        setColorWheelEnabled={setColorWheelEnabled}
+        colorWheelEnabled={colorWheelEnabled}
       />
       <Palette
         settingValue={settingValue}
         setting={setting}
         variation={variation}
         hex={hex}
+        colorWheelEnabled={colorWheelEnabled}
       />
     </div>
   );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Navbar, Form, Nav, FormControl, NavDropdown } from "react-bootstrap";
+import { Navbar, Form, Nav, FormControl, NavDropdown, Popover } from "react-bootstrap";
+//import Popover from "framework7-react";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Button from 'react-bootstrap/Button';
+//import spectrum from "spectrum-colorpicker2";
+import $ from "jquery";
+import ColorWheelPicker from "./ColorWheelPicker.js";
+import * as FlexiColorPicker from "./colorpicker.js"
 
 const tinycolor = require("tinycolor2");
 
@@ -8,6 +15,8 @@ export default function Settings({
   changeVariation,
   changeSetting,
   changeSettingValue,
+  setColorWheelEnabled,
+  colorWheelEnabled
 }) {
   const handleHexChange = e => {
     e.preventDefault();
@@ -31,6 +40,56 @@ export default function Settings({
   const handleSettingValueChange = e => {
     changeSettingValue(e);
   };
+
+  const getColorPickerButtonText = function() {
+    if (colorWheelEnabled) {
+      return "Disable color picker wheel";
+    } else {
+      return "Enable color picker wheel";
+    }
+  }
+
+  const toggleColorWheelState = function() {
+    if (!colorWheelEnabled) {
+      setColorWheelEnabled(true);
+    } else {
+      setColorWheelEnabled(false);
+    }
+  }
+
+  const tieToSpectrum = function() {
+    $('#color-picker').spectrum({
+      type: "component"
+    });    
+  }
+
+  const handleColorPickerOpenedEvent = e => {
+    console.log("color picker opened completed!!");
+  };
+  
+  const colorWheelPopOverHandle = (
+    <Popover id="popover-basic">
+      <Popover.Title>Pick a color!</Popover.Title>
+      <Popover.Content>
+        <input id="color-picker" defaultValue='#276cb8' />
+        <div id="picker">Does this work?</div>
+        <div id="slide">Does this work?</div>
+      </Popover.Content>
+      <ColorWheelPicker></ColorWheelPicker>
+    </Popover>
+  );
+  
+  /*
+ const colorWheelPopOverHandle = (
+  <PopoverColorPicker id="popover-basic">
+    <PopoverColorPicker.Title>Pick a color!</PopoverColorPicker.Title>
+    <PopoverColorPicker.Content>
+      <input id="color-picker" defaultValue='#276cb8' />
+    </PopoverColorPicker.Content>
+  </PopoverColorPicker>
+);
+*/
+
 
   return (
     <div>
@@ -89,6 +148,7 @@ export default function Settings({
             <Nav.Link value={"greyscale"} onClick={e => changeSettings(e)}>
               Greyscale
             </Nav.Link>
+
             <NavDropdown
               onSelect={e => handleSettingValueChange(e)}
               title="Change Value (%)"
@@ -104,6 +164,13 @@ export default function Settings({
               <NavDropdown.Item eventKey="40">90</NavDropdown.Item>
               <NavDropdown.Item eventKey="50">100</NavDropdown.Item>
             </NavDropdown>
+
+            <OverlayTrigger trigger="click" placement="right" overlay={colorWheelPopOverHandle}>
+              <Nav.Link id="ColorPickerWheelController" onClick={e => toggleColorWheelState(e)}>
+                  {getColorPickerButtonText()}
+              </Nav.Link>
+            </OverlayTrigger>
+
           </Navbar.Collapse>
         </Nav>
         <Nav.Link id="random" onClick={() => setRandom()}>
@@ -116,3 +183,5 @@ export default function Settings({
     </div>
   );
 }
+
+
